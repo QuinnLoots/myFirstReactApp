f1ae6205947f324a82bbfda6c05796936d488c38
Quinn Loots <loots.quinn@gmail.com>
2019-02-18 17:07:50 +0100

    mysecondreactapp

---
+++ mysecondreactapp/src/components/SearchBar.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+class SearchBar extends Component {
+  state = { term: "" };
+
+  onFormSubmit = event => {
+    event.preventDefault();
+  };
+  render() {
+    return (
+      <div className="ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Image search</label>
+            <input
+              value={this.state.term}
+              onChange={e => this.setState({ term: e.target.value })}
+              type="text"
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
